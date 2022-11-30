80b98f82cc7227897ed6481f3aac7cab39a12441
RafaMazoliniF <102422449+RafaMazoliniF@users.noreply.github.com>
2022-11-29 22:29:37 -0300

    Update sudoku.asm

--- sudoku.asm
+++ sudoku.asm
@@ -8,53 +8,6 @@
         mov al, cor ;define a cor
         int 10h
     endm
-    
-    printaLinha macro Xi, Yi, quantidade, direcao, cor
-        mov cx, Xi
-        mov dx, Yi
-        mov bl, quantidade
-        mov si, direcao
-        
-        cmp bh, 0
-        jz direita
-        cmp bh, 1
-        jz esquerda
-        cmp bh, 2
-        jz cima
-        cmp bh, 3
-        jz baixo
-        
-        direita:
-            printaPonto cx, dx, cor
-            inc cx
-            dec bl
-            jnz direita
-            jmp fim_linha
-            
-        esquerda:
-            printaPonto cx, dx, cor
-            dec cx
-            dec bl
-            jnz esquerda
-            jmp fim_linha
-        
-        cima:
-            printaPonto cx, dx, cor
-            dec dx
-            dec bl
-            jnz cima
-            jmp fim_linha
-        
-        baixo:
-            printaPonto cx, dx, cor
-            inc dx
-            dec bl
-            jnz baixo
-            jmp fim_linha
-        
-        fim_linha:
-    
-    endm
 
 .stack 100h
 .data
@@ -73,10 +26,102 @@ main proc
     mov al, 12h ;
     int 10h
     
-    printaLinha 1, 100, 10, 3, 3
+    ;S do sudoku
+    mov cx, 10
+    mov dx, 200
+    mov bl, 15
+    mov quantidadeLinha, 30
+    mov direcaoLinha, 0
+    call printaLinha
+    
+    mov cx, 10
+    mov dx, 200
+    mov bl, 15
+    mov quantidadeLinha, 30
+    mov direcaoLinha, 3
+    call printaLinha
+    
+    mov cx, 10
+    mov dx, 230
+    mov bl, 15
+    mov quantidadeLinha, 30
+    mov direcaoLinha, 0
+    call printaLinha
+    
+    mov cx, 40
+    mov dx, 230
+    mov bl, 15
+    mov quantidadeLinha, 30
+    mov direcaoLinha, 3
+    call printaLinha
+    
+    mov cx, 40
+    mov dx, 260
+    mov bl, 15
+    mov quantidadeLinha, 30
+    mov direcaoLinha, 1
+    call printaLinha
+    
+    ;U do sudoku
+    mov cx, 40
+    mov dx, 230
+    mov bl, 15
+    mov quantidadeLinha, 30
+    mov direcaoLinha, 3
+    call printaLinha
     
     ;termina o programa
     mov ah, 4ch
     int 21h
 main endp
+
+printaLinha proc 
+        ; cx = X inicial
+        ; dx = Y inicial
+        ; bl = cor
+        ; quantidadeLinha = n de pixels printados
+        ; direcaoLinha = direção que a reta toma
+        
+        cmp direcaoLinha, 0
+        jz direita
+        cmp direcaoLinha, 1
+        jz esquerda
+        cmp direcaoLinha, 2
+        jz cima
+        cmp direcaoLinha, 3
+        jz baixo
+        
+        direita:
+            printaPonto cx, dx, bl
+            inc cx
+            dec quantidadeLinha
+            jnz direita
+            jmp fim_linha
+            
+        esquerda:
+            printaPonto cx, dx, bl
+            dec cx
+            dec quantidadeLinha
+            jnz esquerda
+            jmp fim_linha
+        
+        cima:
+            printaPonto cx, dx, bl
+            dec dx
+            dec quantidadeLinha
+            jnz cima
+            jmp fim_linha
+        
+        baixo:
+            printaPonto cx, dx, bl
+            inc dx
+            dec quantidadeLinha
+            jnz baixo
+            jmp fim_linha
+        
+        fim_linha:
+        
+        ret
+    printaLinha endp
+
 end main
